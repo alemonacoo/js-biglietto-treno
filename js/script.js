af395db67a7eaa615edb087ed1f5f48b0fdcb6ac
alemonacoo <ale.monaco@outlook.com>
2022-08-23 16:04:10 +0200

    Adding prompts, algorithm and output

---
+++ js/script.js
@@ -0,0 +1,25 @@
+// First prompt
+const age = parseInt(prompt("Quanti anni hai?"));
+console.log("age", age);
+
+// Second prompt
+const distance = parseInt(prompt("Quanti Km vuoi percorrere?"));
+console.log("distance", distance);
+
+// Conditions and output
+if (!isNaN(age) && !isNaN(distance)) {
+  let ticketPrice = 0.21 * distance;
+  let discountedPrice;
+  if (age <= 18) {
+    discountedPrice = ticketPrice * 0.2;
+  } else if (age >= 18) {
+    discountedPrice = ticketPrice * 0.4;
+  } else {
+    discountedPrice = ticketPrice;
+  }
+  console.log(
+    `Il tuo biglietto verrà a costare: ${discountedPrice.toFixed(2)} Euro`
+  );
+} else {
+  console.log("Per favore, inserire valori numerici");
+}
