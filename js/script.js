2d12edf9009a9ed53cd9ea63d2597d17bb295d66
alemonacoo <ale.monaco@outlook.com>
2022-08-25 09:36:09 +0200

    fixing discounted price 2

--- js/script.js
+++ js/script.js
@@ -9,14 +9,14 @@ console.log("distance", distance);
 // Conditions and output
 if (!isNaN(age) && !isNaN(distance)) {
   let ticketPrice = 0.21 * distance;
+  let discount = 0;
   let discountedPrice;
-  if (age <= 18) {
-    discountedPrice = ticketPrice - ticketPrice * 0.2;
-  } else if (age >= 18) {
-    discountedPrice = ticketPrice - ticketPrice * 0.4;
-  } else {
-    discountedPrice = ticketPrice;
+  if (age < 18) {
+    discount = ticketPrice * 0.2;
+  } else if (age > 65) {
+    discount = ticketPrice * 0.4;
   }
+  discountedPrice = ticketPrice - discount;
   console.log(
     `Il tuo biglietto verrà a costare: ${discountedPrice.toFixed(2)} Euro`
   );
